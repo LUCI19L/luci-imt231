d4dec86d1b21c2a16dadcfaaba88ee4398d9c32b
LUCI19L <luciguevarahuaquipa@gmail.com>
2025-03-03 22:16:46 -0400

    actividades

---
+++ tpPracticoPermisos/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pedir el nombre del usuario
+echo "ingresa tu nombre, porfavor:"
+read -r nombre
+
+# Saludar al usuario
+echo "¡Bienvenido a script, $nombre!"
+
+# Mostrar el contenido del script
+echo "El contenido del script es:"
+cat script.sh
